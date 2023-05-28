360bd0f7d03e4b7fbeb2f5a4a3997bed9a9b2669
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-28 14:31:25 -0400

    Install dependencies and set up server

---
+++ knex/knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'pet_furriendly_db',
+      user:     'postgres',
+      password: 'Turing2301',
+      port: 5432
+    }
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
