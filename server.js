c94d931d734984a366f5a02aa95f99e76085c4d7
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-28 14:38:25 -0400

    Update file path for knex import

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const knex = require('./knex/knexfile.js');
+const knex = require('./knex/knex');
 
 app.set('port', 8080);
 
